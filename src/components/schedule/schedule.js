36e051610dfc59d41dfd5c2c5c2aa5b313086df1
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-04 20:17:22 -0500

    Create schedule.js

---
+++ src/components/schedule/schedule.js
@@ -0,0 +1,13 @@
+// components/Schedule.js
+import React from 'react';
+
+const Schedule = () => {
+  return (
+    <div>
+      <h2>Schedule Component</h2>
+      {/* Add schedule-related UI here */}
+    </div>
+  );
+};
+
+export default Schedule;
