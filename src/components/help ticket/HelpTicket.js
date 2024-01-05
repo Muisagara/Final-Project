5e819a98bb765eda4bd6e33e20e28d74bc995b44
Daniel Zavala <zavaladaniel151@gmail.com>
2024-01-04 20:17:47 -0500

    Create HelpTicket.js

---
+++ src/components/help ticket/HelpTicket.js
@@ -0,0 +1,13 @@
+// components/HelpTicket.js
+import React from 'react';
+
+const HelpTicket = () => {
+  return (
+    <div>
+      <h2>Help Ticket Component</h2>
+      {/* Add help ticket-related UI here */}
+    </div>
+  );
+};
+
+export default HelpTicket;
